7666a8f39bcf53275b1c981c9a96e0564f71f188
venakatasatya.tutika <anilkumartutika000@gmail.com>
2019-08-22 10:08:43 +0530

    fetching data is done

--- src/main/java/com/ruchik/streamsDemo/ApiCalls/MakeCalls.java
+++ src/main/java/com/ruchik/streamsDemo/ApiCalls/MakeCalls.java
@@ -1,6 +1,5 @@
 package com.ruchik.streamsDemo.ApiCalls;
 
-import org.graalvm.compiler.nodes.calc.ObjectEqualsNode;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,24 +16,25 @@ import java.util.List;
 public class MakeCalls {
     // /statuses/home_timeline.json
     WebClient client = WebClient.builder()
-            .baseUrl("https://api.twitter.com/1.1")
+            .baseUrl("https://newsapi.org/v2/everything?q=mobiles&apiKey=389599afec1e4727a7de75f65b5f050c")
+//           ( in url we can also give &sortBy=publishedAt&from=2019-08-22)
             .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
 //            .defaultHeaders(HttpHeaders.AUTHORIZATION, OAuth)
             .build();
 
-    @GetMapping("/tweets")
+    @GetMapping("/news")
     public Flux<?> getTweets() {
         List<Object> myArray = new ArrayList<>();
         Flux<?> response = Flux.fromIterable(myArray);
 
           return client.get()
-                .uri("/statuses/home_timeline.json")
-                .header("Authorization", "OAuth oauth_consumer_key=\"SOnXMD3VSmGD5KTkj37rSQHAE\",oauth_token=\"3158369077-1QS3eTlW0Q8LvXvlkyPOEAaQaBO6aFVaKZ0tgie\",oauth_signature_method=\"HMAC-SHA1\",oauth_timestamp=\"1566283643\",oauth_nonce=\"51PROn\",oauth_version=\"1.0\",oauth_signature=\"3h%2FFicHF6yb%2BAhTsyFz6uh7tNbg%3D\"")
+//                .uri("/statuses/home_timeline.json")
+//                .header("Authorization", "OAuth oauth_consumer_key=\"SOnXMD3VSmGD5KTkj37rSQHAE\",oauth_token=\"3158369077-1QS3eTlW0Q8LvXvlkyPOEAaQaBO6aFVaKZ0tgie\",oauth_signature_method=\"HMAC-SHA1\",oauth_timestamp=\"1566283643\",oauth_nonce=\"51PROn\",oauth_version=\"1.0\",oauth_signature=\"3h%2FFicHF6yb%2BAhTsyFz6uh7tNbg%3D\"")
                     .retrieve()
-                  .bodyToFlux(Object.class);
-//                .exchange().filter(a -> myArray.add(a));
+                  .bodyToFlux(Object.class)
+                    .filter(a -> myArray.add(a));
 //                .flatMapMany(clientResponse -> myArray.add(clientResponse.bodyToMono(Object.class)));
-
+//        System.out.println(response);
 //          return  response;
     }
 
